496c05ef3924c94d7cee40116a69c8bef48bc4ac
copy <xiaoxinxing12@qq.com>
2016-06-07 21:15:20 +0800

    Initial commit

---
+++ app/src/main/java/org/chzz/library/demo/MainActivity.java
@@ -0,0 +1,94 @@
+/*
+ *   Copyright (C)  2016 android@19code.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.chzz.library.demo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import org.chzz.library.L;
+import org.chzz.library.demo.ui.activity.AppManagerActivity;
+import org.chzz.library.demo.ui.activity.DeviceActivity;
+import org.chzz.library.demo.ui.activity.VerificationActivity;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "ghost";
+    private Context c = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button virification = (Button) findViewById(R.id.virification);
+        Button deviceutils = (Button) findViewById(R.id.deviceutils);
+        Button systemutils = (Button) findViewById(R.id.systemutils);
+        Button netutils = (Button) findViewById(R.id.netutils);
+        Button fileutils = (Button) findViewById(R.id.fileutils);
+        Button logutils = (Button) findViewById(R.id.logutils);
+        Button apputils = (Button) findViewById(R.id.apputils);
+        systemutils.setOnClickListener(this);
+        netutils.setOnClickListener(this);
+        fileutils.setOnClickListener(this);
+        logutils.setOnClickListener(this);
+        apputils.setOnClickListener(this);
+        deviceutils.setOnClickListener(this);
+        virification.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.apputils:
+                startActivity(new Intent(MainActivity.this, AppManagerActivity.class));
+                break;
+            case R.id.deviceutils:
+                startActivity(new Intent(MainActivity.this, DeviceActivity.class));
+                break;
+            case R.id.logutils:
+                testLog();
+                break;
+            case R.id.fileutils:
+                break;
+            case R.id.netutils:
+                break;
+            case R.id.systemutils:
+                break;
+            case R.id.virification:
+                startActivity(new Intent(MainActivity.this, VerificationActivity.class));
+                break;
+        }
+    }
+
+    private String xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><!--  Copyright w3school.com.cn --><note><to>George</to><from>John</from><heading>Reminder</heading><body>Don't forget the meeting!</body></note>";
+    private String json = "{'type1': {'0': {'age': 12,'name': 'zhangsdan'},'1': {'age': 13,'name': 'lisi'},'num': '123'},'type3': {'0': {'age': 14,'name': 'wangwu'},'1': {'age': 15,'name': 'maliu'},'num': '456',}}";
+
+    private void testLog() {
+        L.init(true, "admin");
+        L.v("Verbose...");
+        L.d("Debug...");
+        L.i("info。。。");
+        L.w("Warn...");
+        L.e("Error...");
+        L.a("ASSERT...");
+        L.json(json);
+        L.xml(xml);
+    }
+}
---
+++ app/src/main/java/org/chzz/library/demo/ui/activity/DeviceActivity.java
@@ -0,0 +1,89 @@
+/*
+ *   Copyright (C)  2016 android@19code.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package org.chzz.library.demo.ui.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.chzz.library.DensityUtil;
+import org.chzz.library.DeviceUtils;
+import org.chzz.library.SystemUtils;
+import org.chzz.library.demo.R;
+
+
+public class DeviceActivity extends AppCompatActivity {
+    private Context c;
+    private TextView mDeviceutilstextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device);
+        c = this;
+        mDeviceutilstextview = (TextView) findViewById(R.id.deviceutils_textview);
+        initDeviecesInfos();
+    }
+
+    private void initDeviecesInfos() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("AndroidID--" + DeviceUtils.getAndroidID(c) + "\n");
+        sb.append("getIMSI--" + DeviceUtils.getIMEI(c) + "\n");
+        sb.append("getIMSI--" + DeviceUtils.getIMSI(c) + "\n");
+        sb.append("getWifiMacAddr--" + DeviceUtils.getWifiMacAddr(c) + "\n");
+        //sb.append("getIP--" + DeviceUtils.getIP(c) + "\n");
+        sb.append("getSerial--" + DeviceUtils.getSerial() + "\n");
+        sb.append("getSIMSerial--" + DeviceUtils.getSIMSerial(c) + "\n");
+        sb.append("getMNC--" + DeviceUtils.getMNC(c) + "\n");
+        sb.append("getBuildBrand--" + DeviceUtils.getBuildBrand() + "\n");
+        sb.append("getBuildHost--" + DeviceUtils.getBuildHost() + "\n");
+        sb.append("getBuildTags--" + DeviceUtils.getBuildTags() + "\n");
+        sb.append("getBuildTime--" + DeviceUtils.getBuildTime() + "\n");
+        sb.append("getBuildUser--" + DeviceUtils.getBuildUser() + "\n");
+        sb.append("getBuildVersionRelease--" + DeviceUtils.getBuildVersionRelease() + "\n");
+        sb.append("getBuildVersionCodename--" + DeviceUtils.getBuildVersionCodename() + "\n");
+        sb.append("getBuildVersionIncremental--" + DeviceUtils.getBuildVersionIncremental() + "\n");
+        sb.append("getBuildVersionSDK--" + DeviceUtils.getBuildVersionSDK() + "\n");
+        sb.append("getSupportedABIS--" + DeviceUtils.getSupportedABIS()[0] + DeviceUtils.getSupportedABIS()[1] + "\n");
+        sb.append("getManufacturer--" + DeviceUtils.getManufacturer() + "\n");
+        sb.append("getBootloader--" + DeviceUtils.getBootloader() + "\n");
+        sb.append("getScreenDisplayID--" + DeviceUtils.getScreenDisplayID(c) + "\n");
+        sb.append("getDisplayVersion--" + DeviceUtils.getDisplayVersion() + "\n");
+        sb.append("getLanguage--" + DeviceUtils.getLanguage() + "\n");
+        sb.append("getCountry--" + DeviceUtils.getCountry(c) + "\n");
+        sb.append("getOSVersion--" + DeviceUtils.getOSVersion() + "\n");
+        //sb.append("getGSFID--" + DeviceUtils.getGSFID(c) + "\n");
+        sb.append("getBluetoothMAC--" + DeviceUtils.getBluetoothMAC(c) + "\n");
+        sb.append("getPsuedoUniqueID--" + DeviceUtils.getPsuedoUniqueID() + "\n");
+        sb.append("getFingerprint--" + DeviceUtils.getFingerprint() + "\n");
+        sb.append("getHardware--" + DeviceUtils.getHardware() + "\n");
+        sb.append("getProduct--" + DeviceUtils.getProduct() + "\n");
+        sb.append("getDevice--" + DeviceUtils.getDevice() + "\n");
+        sb.append("getBoard--" + DeviceUtils.getBoard() + "\n");
+        sb.append("getRadioVersion--" + DeviceUtils.getRadioVersion() + "\n");
+        sb.append("getUA--" + DeviceUtils.getUA(c) + "\n");
+        sb.append("getDensity--" + DeviceUtils.getDensity(c) + "\n");
+        //sb.append("getAccounts--" + DeviceUtils.getGoogleAccounts(c)[0] + "\n");
+        sb.append("isRunningOnEmulator--" + SystemUtils.isRunningOnEmulator() + "\n");
+        sb.append("isRooted--" + SystemUtils.isRooted() + "\n");
+        sb.append("ScreenWidth x ScreenHeight--" + DensityUtil.getScreenW(c) + "x" + (DensityUtil.getScreenRealH(c)) + "\n");
+        Log.i("ghost", "StatusBar:" + DensityUtil.getStatusBarH(c) + ",Nav:" + DensityUtil.getNavigationBarrH(c));
+        mDeviceutilstextview.setText(sb);
+    }
+}
